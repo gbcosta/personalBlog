6ae4dca156c69cf6a5a1f3bf262c113f15dc37f4
gbcostasant@gmail.com <gbcostasant@gmail.com>
2023-07-12 19:51:03 -0300

    add blog

---
+++ src/components/blog/blog.tsx
@@ -0,0 +1,7 @@
+const Blog = () =>{
+    return <div>
+        <h2 className={"text-4xl text-white font-bold mb-8"}>Blog</h2>
+    </div>;
+}
+
+export default Blog;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/header";
 import Projects from "../../components/projects/projects";
 import Profile from "../../components/profile/profile";
+import Blog from "../../components/blog/blog";
 
 const HomePage = () =>{
     return <div className={"flex flex-col"}>
         <Header />
         <Profile />
+        <Blog />
         <Projects />
 
     </div>
