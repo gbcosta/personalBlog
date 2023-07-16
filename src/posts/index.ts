74e2d34d2d8d3319dd3f5f984b5be09666d503c7
gbcostasant@gmail.com <gbcostasant@gmail.com>
2023-07-16 18:07:55 -0300

    update to be private

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,23 +1,9 @@
-import post_01 from "./post_01.ts";
-import post_02 from "./post_02.ts";
-import post_03 from "./post_03.ts";
-import post_04 from "./post_04.ts";
-import post_05 from "./post_05.ts";
-import post_06 from "./post_06.ts";
 import { PostData } from "../utils/utils.tsx";
 
 type PostsData = Array<PostData>;
 
 
-const posts: PostsData = [
-    post_01,
-    post_02,
-    post_03,
-    post_04,
-    post_05,
-    post_06,
-
-];
+const posts: PostsData = [];
 
 const sortByDate = () =>{
     const sortedRepos = posts.sort((postA: PostData , postB: PostData)=>{
